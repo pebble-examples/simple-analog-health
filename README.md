6814f305003e778e893fd8459a592571cc662dd8
Jon Barlow <orviwan@gmail.com>
2016-01-25 12:22:53 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# simple-analog-health
+
+![screenshot](simple-analog-health-screenshot.png)
+
+Example analog watchface that uses hands instead of numbers and displays the step count from the Pebble Health API.
---
+++ src/simple_analog.c
@@ -0,0 +1,171 @@
+#include "simple_analog.h"
+
+#include "pebble.h"
+
+static Window *s_window;
+static Layer *s_simple_bg_layer, *s_date_layer, *s_hands_layer;
+static TextLayer *s_label, *s_num_label;
+
+static GPath *s_tick_paths[NUM_CLOCK_TICKS];
+static GPath *s_minute_arrow, *s_hour_arrow;
+
+static bool s_health_available;
+
+static void health_handler(HealthEventType event, void *context) {
+  static char s_value_buffer[8];
+  if (event == HealthEventMovementUpdate) {
+    // display the step count
+    snprintf(s_value_buffer, sizeof(s_value_buffer), "%d", (int)health_service_sum_today(HealthMetricStepCount));
+    text_layer_set_text(s_num_label, s_value_buffer);
+  }
+}
+
+static void bg_update_proc(Layer *layer, GContext *ctx) {
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
+    const int x_offset = PBL_IF_ROUND_ELSE(18, 0);
+    const int y_offset = PBL_IF_ROUND_ELSE(6, 0);
+    gpath_move_to(s_tick_paths[i], GPoint(x_offset, y_offset));
+    gpath_draw_filled(ctx, s_tick_paths[i]);
+  }
+}
+
+static void hands_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  GPoint center = grect_center_point(&bounds);
+
+  const int16_t second_hand_length = PBL_IF_ROUND_ELSE((bounds.size.w / 2) - 19, bounds.size.w / 2);
+
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  int32_t second_angle = TRIG_MAX_ANGLE * t->tm_sec / 60;
+  GPoint second_hand = {
+    .x = (int16_t)(sin_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.y,
+  };
+
+  // second hand
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_line(ctx, second_hand, center);
+
+  // minute/hour hand
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+
+  gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
+  gpath_draw_filled(ctx, s_minute_arrow);
+  gpath_draw_outline(ctx, s_minute_arrow);
+
+  gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
+  gpath_draw_filled(ctx, s_hour_arrow);
+  gpath_draw_outline(ctx, s_hour_arrow);
+
+  // dot in the middle
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, GRect(bounds.size.w / 2 - 1, bounds.size.h / 2 - 1, 3, 3), 0, GCornerNone);
+}
+
+static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
+  layer_mark_dirty(window_get_root_layer(s_window));
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_simple_bg_layer = layer_create(bounds);
+  layer_set_update_proc(s_simple_bg_layer, bg_update_proc);
+  layer_add_child(window_layer, s_simple_bg_layer);
+
+  s_date_layer = layer_create(bounds);
+  layer_add_child(window_layer, s_date_layer);
+
+  s_label = text_layer_create(PBL_IF_ROUND_ELSE(
+    GRect(53, 114, 37, 20),
+    GRect(36, 114, 37, 20)));
+  text_layer_set_text(s_label, "steps");
+  text_layer_set_background_color(s_label, GColorBlack);
+  text_layer_set_text_color(s_label, GColorWhite);
+  text_layer_set_font(s_label, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  layer_add_child(s_date_layer, text_layer_get_layer(s_label));
+
+  s_num_label = text_layer_create(PBL_IF_ROUND_ELSE(
+    GRect(90, 114, 58, 20),
+    GRect(73, 114, 58, 20)));
+  text_layer_set_background_color(s_num_label, GColorBlack);
+  text_layer_set_text_color(s_num_label, GColorWhite);
+  text_layer_set_font(s_num_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  layer_add_child(s_date_layer, text_layer_get_layer(s_num_label));
+
+  s_hands_layer = layer_create(bounds);
+  layer_set_update_proc(s_hands_layer, hands_update_proc);
+  layer_add_child(window_layer, s_hands_layer);
+
+  // subscribe to health events
+  s_health_available = health_service_events_subscribe(health_handler, NULL);
+  if(s_health_available) {
+    // force initial steps display
+    health_handler(HealthEventMovementUpdate, NULL);
+  }
+  else {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Health not available!");
+  }
+
+}
+
+static void window_unload(Window *window) {
+  health_service_events_unsubscribe();
+
+  layer_destroy(s_simple_bg_layer);
+  layer_destroy(s_date_layer);
+
+  text_layer_destroy(s_label);
+  text_layer_destroy(s_num_label);
+
+  layer_destroy(s_hands_layer);
+}
+
+static void init() {
+  s_window = window_create();
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  window_stack_push(s_window, true);
+
+  // init hand paths
+  s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
+  s_hour_arrow = gpath_create(&HOUR_HAND_POINTS);
+
+  Layer *window_layer = window_get_root_layer(s_window);
+  GRect bounds = layer_get_bounds(window_layer);
+  GPoint center = grect_center_point(&bounds);
+  gpath_move_to(s_minute_arrow, center);
+  gpath_move_to(s_hour_arrow, center);
+
+  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
+    s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS[i]);
+  }
+
+  tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
+}
+
+static void deinit() {
+  gpath_destroy(s_minute_arrow);
+  gpath_destroy(s_hour_arrow);
+
+  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
+    gpath_destroy(s_tick_paths[i]);
+  }
+
+  tick_timer_service_unsubscribe();
+  window_destroy(s_window);
+}
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
